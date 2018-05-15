c401fac14426060df904577f6fcf89e3b4533f93
PPakalns <peterispakalns@gmail.com>
2018-05-15 22:07:44 +0300

    3 channel color fix x2

--- gan.py
+++ gan.py
@@ -138,8 +138,10 @@ class GAN(CNN):
         # Rescale images 0 - 1
         simg = simg * 0.5 + 0.5
         gimg = gimg * 0.5 + 0.5
+        dimg = dimg * 0.5 + 0.5
         np.clip(simg, 0, 1, out=simg)
         np.clip(gimg, 0, 1, out=gimg)
+        np.clip(dimg, 0, 1, out=dimg)
 
         fig, axs = plt.subplots(cnt, 3)
         for i in range(cnt):
